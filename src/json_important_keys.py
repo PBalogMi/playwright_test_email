c04da3fb74eaf815dc6319a22d8911ef247daf73
PBalogMi <pbalogmi@gmail.com>
2023-12-11 13:03:45 +0100

    doc strings correction

--- src/json_important_keys.py
+++ src/json_important_keys.py
@@ -2,7 +2,12 @@ import os
 import json
 
 
-def check_json_keys():
+def check_json_keys() -> bool:
+    """
+    Checks if all required keys are present in a JSON file and reads a JSON file containing shared credentials.
+
+    :return bool: True if any required key is missing, False otherwise.
+    """
     required_keys = ["email", "password", "account_name"]
     working_directory = os.getcwd()
     path_to_file = os.path.join(working_directory, 'files/json_shared_credentials/json_shared_credentials.json')
